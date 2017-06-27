eb2c3ad59190c630fa7a0435038017cc49c19d4a
mdaisuke <daisuke.mori9@gmail.com>
2017-06-27 10:29:30 +0900

    updaterd

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,1 +1,29 @@
 package lexer
+
+import (
+	"testing"
+
+	"github.com/mdaisuke/prattparser/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `(),=+-*/^~!?:`
+
+	tests := []struct {
+		expectedType string
+		expectedText string
+	}{
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.MINUS, "-"},
+		{token.ASTERISK, "*"},
+		{token.SLASH, "/"},
+		{token.CARET, "^"},
+		{token.TILDA, "~"},
+		{token.BANG, "!"},
+		{token.RPAREN, "?"},
+		{token.COLON, ":"},
+	}
+}
